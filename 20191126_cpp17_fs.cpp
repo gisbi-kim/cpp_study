f3756f1b1faaae0b2cf730ec48637a32f9c9c729
Giseop Kim <kkissbee@kaist.ac.kr>
2019-11-26 15:55:39 +0900

    Create 20191126_cpp17_fs.cpp

---
+++ 20191126_cpp17_fs.cpp
@@ -0,0 +1,35 @@
+// This file is a "Hello, world!" in C++ language by GCC for wandbox.
+#include <iostream>
+#include <cstdlib>
+#include <filesystem>
+
+using std::cout;
+using std::endl;
+namespace fs = std::filesystem;
+
+int main()
+{
+    std::cout << "Hello, Wandbox!" << std::endl;
+    
+//    std::filesystem::path p1(L"/usr");
+    fs::path p1(L"/usr");
+    cout << p1 << endl;
+    cout << p1.c_str() << endl;
+    
+    decltype(p1) p1_2 = p1;
+    cout << p1_2 << endl;
+
+    auto p1_3 = p1;
+    cout << p1_3 << endl;
+    
+    fs::space_info s1 = space(p1);
+    cout << s1.capacity << endl;
+    cout << s1.free << endl;
+    
+    for(auto& _p: fs::directory_iterator(p1))
+    {
+        cout << _p.path() << '\n';
+    }
+    
+
+}
