83f921c576492782c64d4d3a8da14288291cde7f
Giseop Kim <kkissbee@kaist.ac.kr>
2023-04-08 23:20:59 +0900

    Add files via upload

---
+++ 20221119_cpp20_module/hello/build/CMakeFiles/main.dir/main.cpp.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/main.dir/main.cpp.o: \
+ /home/gskim/Documents/practices/module/with_cmake/main.cpp \
+ /usr/include/stdc-predef.h
+CMakeFiles/main.dir/main.cpp.o: hello.c++m
+CXX_IMPORTS += hello.c++m
---
+++ 20221119_cpp20_module/hello/main.cpp
@@ -0,0 +1,12 @@
+// // clang-format -i -style=Microsoft *.cpp
+
+// // module;
+// import <iostream>;
+
+import hello;
+
+int main(void)
+{
+    greeter("Giseop");
+    return 0;
+}
