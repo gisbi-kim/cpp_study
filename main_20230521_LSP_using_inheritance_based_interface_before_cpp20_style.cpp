04c662a6389012aa9f965e651f91bc149088d37b
Giseop Kim <kkissbee@kaist.ac.kr>
2023-05-23 22:17:27 +0900

    Update main_20230521_LSP_using_inheritance_based_interface_before_cpp20_style.cpp

--- main_20230521_LSP_using_inheritance_based_interface_before_cpp20_style.cpp
+++ main_20230521_LSP_using_inheritance_based_interface_before_cpp20_style.cpp
@@ -2,7 +2,8 @@
  tested at: https://www.onlinegdb.com/online_c++_compiler
   select cpp14 before running 
 */
-
+#include <vector>
+#include <memory>
 #include <iostream>
 
 class Drawable {
@@ -29,11 +30,31 @@ void drawShape(const Drawable& drawable) {
 }
 
 int main() {
-    Circle circle;
-    drawShape(circle);  // Circle에 대한 호출
+    // single test 
+    {
+        Circle circle;
+        drawShape(circle);  // Circle에 대한 호출
+        
+        Rectangular rectangular;
+        drawShape(rectangular);  // Rectangular에 대한 호출
+    }    
+    std::cout << std::endl;
     
-    Rectangular rectangular;
-    drawShape(rectangular);  // Rectangular에 대한 호출
-    
+    // batch test 
+    {
+        std::vector<std::unique_ptr<Drawable>> drawable_objects;
+        
+        drawable_objects.emplace_back(std::make_unique<Circle>());
+        drawable_objects.emplace_back(std::make_unique<Rectangular>());
+        drawable_objects.emplace_back(std::make_unique<Circle>());
+        drawable_objects.emplace_back(std::make_unique<Rectangular>());
+        drawable_objects.emplace_back(std::make_unique<Rectangular>());
+        
+        for(const auto& drawable_obj: drawable_objects) {    
+            drawShape(*drawable_obj); 
+        }
+    }
+    std::cout << std::endl;
+
     return 0;
 }
